b361ea8314c5fa30d3a7d27f1a7a6584e9222863
rockysantiago <raphael.santiago@icloud.com>
2018-07-30 17:10:43 +0800

    add weather reducer

---
+++ src/reducers/weather.js
@@ -0,0 +1,9 @@
+import { FETCH_WEATHER } from 'actions/types';
+
+export default (state = null, action) => {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return action.payload;
+  }
+  return state;
+};
